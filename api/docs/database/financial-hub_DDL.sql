64f87293387549a03febddc020bbd7f2b129f642
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2021-10-12 17:50:51 -0300

    Add : DDL Script

---
+++ api/docs/database/financial-hub_DDL.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE financial_hub;
+USE financial_hub;
+
+CREATE TABLE accounts(
+  id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWSEQUENTIALID(),
+  name VARCHAR(200) NOT NULL,
+  description VARCHAR(500) NULL,
+  currency VARCHAR(50),
+  active BIT DEFAULT 1
+);
