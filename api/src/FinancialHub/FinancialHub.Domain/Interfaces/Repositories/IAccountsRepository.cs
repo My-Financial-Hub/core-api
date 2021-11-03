1450372a96c643e0fea154f112644a3cf4ff2c30
Franklin Bitencourt Dantas <franklin.bdantas@hotmail.com>
2021-11-03 12:52:16 -0300

    Revert "Feature/categories"

--- api/src/FinancialHub/FinancialHub.Domain/Interfaces/Repositories/IAccountsRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using FinancialHub.Domain.Entities;
-
-namespace FinancialHub.Domain.Interfaces.Repositories
-{
-    public interface IAccountsRepository : IBaseRepository<AccountEntity>
-    {
-        //Task<ICollection<<AccountEntity>> GetByUserId(string userId);
-    }
-}
