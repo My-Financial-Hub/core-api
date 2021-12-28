2a0e838e74dec27ed15f6892ff6361a17cd70744
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2021-12-27 21:48:05 -0300

    Change : CategoriesService Result

--- api/src/FinancialHub/FinancialHub.Services/Services/CategoriesService.cs
+++ api/src/FinancialHub/FinancialHub.Services/Services/CategoriesService.cs
@@ -3,6 +3,7 @@ using FinancialHub.Domain.Entities;
 using FinancialHub.Domain.Interfaces.Services;
 using FinancialHub.Domain.Interfaces.Repositories;
 using FinancialHub.Domain.Interfaces.Mappers;
+using FinancialHub.Domain.Results;
 
 namespace FinancialHub.Services.Services
 {
@@ -17,7 +18,7 @@ namespace FinancialHub.Services.Services
             this.repository = repository;
         }
 
-        public async Task<CategoryModel> CreateAsync(CategoryModel category)
+        public async Task<ServiceResult<CategoryModel>> CreateAsync(CategoryModel category)
         {
             var entity = mapper.Map<CategoryEntity>(category);
 
@@ -26,27 +27,30 @@ namespace FinancialHub.Services.Services
             return mapper.Map<CategoryModel>(entity);
         }
 
-        public async Task<int> DeleteAsync(Guid id)
+        public async Task<ServiceResult<int>> DeleteAsync(Guid id)
         {
             return await this.repository.DeleteAsync(id);
         }
 
-        public async Task<ICollection<CategoryModel>> GetAllByUserAsync(string userId)
+        public async Task<ServiceResult<ICollection<CategoryModel>>> GetAllByUserAsync(string userId)
         {
             var entities = await this.repository.GetAllAsync();
-            return mapper.Map<ICollection<CategoryModel>>(entities);
+
+            var list = this.mapper.Map<ICollection<CategoryModel>>(entities);
+
+            return list.ToArray();
         }
 
-        public async Task<CategoryModel> UpdateAsync(Guid id, CategoryModel category)
+        public async Task<ServiceResult<CategoryModel>> UpdateAsync(Guid id, CategoryModel category)
         {
             var entity = await this.repository.GetByIdAsync(id);
 
             if (entity == null)
             {
-                throw new NullReferenceException($"Not found category with id {id}");
+                return new ServiceError(404, $"Not found category with id {id}");
             }
-            entity.Id = id;
 
+            entity = this.mapper.Map<CategoryEntity>(category);
             entity = await this.repository.UpdateAsync(entity);
 
             return mapper.Map<CategoryModel>(entity);
