be194ac65fcd3d737c8702374bb1c2fa2c41dfb5
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2021-12-28 11:54:04 -0300

    Add : TransactionsService unit tests

---
+++ api/src/FinancialHub/FinancialHub.Services.NUnitTests/Services/Transactions/TransactionsServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using FinancialHub.Domain.Interfaces.Mappers;
+using FinancialHub.Domain.Interfaces.Repositories;
+using FinancialHub.Domain.Interfaces.Services;
+using FinancialHub.Domain.NUnitTests.Generators;
+using FinancialHub.Services.Mappers;
+using FinancialHub.Services.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace FinancialHub.Services.NUnitTests.Services
+{
+    public partial class TransactionsServiceTests
+    {
+        protected Random random;
+        protected EntityGenerator entityGenerator; 
+        protected ModelGenerator modelGenerator; 
+        
+        private ITransactionsService service;
+
+        private IMapper mapper;
+        private Mock<IMapperWrapper> mapperWrapper;
+        private Mock<ITransactionsRepository> repository;
+
+        private void MockMapper()
+        {
+            mapper = new MapperConfiguration(mc =>
+                {
+                    mc.AddProfile(new FinancialHubAutoMapperProfile());
+                }
+            ).CreateMapper();
+
+            this.mapperWrapper = new Mock<IMapperWrapper>();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            this.MockMapper();
+
+            this.repository = new Mock<ITransactionsRepository>();
+            this.service = new TransactionsService(mapperWrapper.Object,repository.Object);
+
+            this.random = new Random();
+
+            this.entityGenerator = new EntityGenerator(random);
+            this.modelGenerator = new ModelGenerator(random);
+        }
+    }
+}
