471bad06c429d2b5714729e31cd9d1115751b254
Franklin Bitencourt Dantas <franklin.bdantas@hotmail.com>
2022-04-12 12:10:39 -0300

    Add : Database Dockerfile
    * Add : Settings using dockerfile config
    
    * Add : Docker connection to DbConnectionStringManager
    
    * Add : OSPlataform check
    
    * Add : Database dockerfile

--- api/src/FinancialHub/FinancialHub.IntegrationTests/Setup/DbConnectionStringManager.cs
+++ api/src/FinancialHub/FinancialHub.IntegrationTests/Setup/DbConnectionStringManager.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace FinancialHub.IntegrationTests.Setup
 {
     internal static class DbConnectionStringManager
     {
-        private const string DockerConnectionString = "Server=localhost;Database=financial_hub;Trusted_Connection=True;";
+        private const string DockerConnectionString = "Server=localhost,1450;Database=financial_hub;user=sa;pwd=P@ssw0rd!;";
         private const string LocalDbConnectionString = "Server=(LocalDB)\\MSSQLLocalDB;Database=financial_hub;Trusted_Connection=True;";
 
         public static string ConnectionString
         {
             get
-            {
-                if (LocalDbExists())
+            {   
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && LocalDbExists())
                 {
                     return LocalDbConnectionString;
                 }
@@ -21,8 +22,8 @@ namespace FinancialHub.IntegrationTests.Setup
             }
         }
         private static readonly string sqlServerLocalDbPath = Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles),
-                "Microsoft SQL Server"
+            Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles),
+            "Microsoft SQL Server"
         );
 
         private static bool LocalDbExists()
