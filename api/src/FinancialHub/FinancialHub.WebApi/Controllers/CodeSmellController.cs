b0dbe817d90bcd789a503b764044d5a49ae872a2
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2022-04-13 14:22:21 -0300

    Code smell test

---
+++ api/src/FinancialHub/FinancialHub.WebApi/Controllers/CodeSmellController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinancialHub.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class CodeSmellController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var name = "aaaa";
+            var res = "".Equals(name) && name.Equals("") && name.Equals(string.Empty);
+            return Ok(res);
+        }
+    }
+}
