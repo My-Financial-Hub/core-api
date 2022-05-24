fc9c1bb0e991dd802413efb94fcacc025e208db6
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2022-05-24 15:46:36 -0300

    Adds : Balance Create/Update Endpoint

--- api/src/FinancialHub/FinancialHub.WebApi/Controllers/BalanceController.cs
+++ api/src/FinancialHub/FinancialHub.WebApi/Controllers/BalanceController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FinancialHub.Domain.Interfaces.Services;
 using FinancialHub.Domain.Models;
 using FinancialHub.Domain.Responses.Success;
+using FinancialHub.Domain.Responses.Errors;
 
 namespace FinancialHub.WebApi.Controllers
 {
@@ -26,7 +28,57 @@ namespace FinancialHub.WebApi.Controllers
         {
             var result = await service.GetAllByAccountAsync(accountId);
 
+            var account = result.Data.FirstOrDefault()?.Account;
+            var total = new BalanceModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Total",
+                Account = account,
+                AccountId = accountId,
+                Amount = result.Data?.Sum(x => x.Amount) ?? 0,
+                IsActive = true
+            };
+
+            result.Data.Add(total);
+
             return Ok(new ListResponse<BalanceModel>(result.Data));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(SaveResponse<BalanceModel>), 200)]
+        [ProducesResponseType(typeof(ValidationErrorResponse), 400)]
+        public async Task<IActionResult> CreateBalance([FromBody] BalanceModel balance)
+        {
+            var result = await this.service.CreateAsync(balance);
+
+            if (result.HasError)
+            {
+                return StatusCode(
+                    result.Error.Code,
+                    new ValidationErrorResponse(result.Error.Message)
+                 );
+            }
+
+            return Ok(new SaveResponse<BalanceModel>(result.Data));
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(typeof(SaveResponse<AccountModel>), 200)]
+        [ProducesResponseType(typeof(NotFoundErrorResponse), 404)]
+        [ProducesResponseType(typeof(ValidationErrorResponse), 400)]
+        public async Task<IActionResult> UpdateBalance([FromRoute] Guid id, [FromBody] BalanceModel balance)
+        {
+            var response = await this.service.UpdateAsync(id, balance);
+
+            if (response.HasError)
+            {
+                return StatusCode(
+                    response.Error.Code,
+                    new ValidationErrorResponse(response.Error.Message)
+                 );
+            }
+
+            return Ok(new SaveResponse<BalanceModel>(response.Data));
         }
 
         [HttpDelete("{id}")]
