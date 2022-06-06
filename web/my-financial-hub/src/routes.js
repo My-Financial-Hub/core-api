937a2b692b1a1e511fe4e2f7f363c37c74d2c399
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2022-06-06 12:36:19 -0300

    Add : Accounts Page Route

--- web/my-financial-hub/src/routes.js
+++ web/my-financial-hub/src/routes.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import App from './pages/App';
 import NotFoundPage from './pages/not-found';
+import AccountsPage from './pages/accounts';
 
 export default function AppRouter() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<App/>}/>
+        <Route path='/accounts' element={<AccountsPage/>}/>
         <Route path='*' element={<NotFoundPage/>}/>
       </Routes>
     </Router>
