81b7d08bc275bc5a3455f4b6251b448dfcac0b2b
Franklin Bitencourt Dantas <franklin.bdantas@hotmail.com>
2022-06-20 16:41:06 -0300

    Add : Default Api Result (#96)

--- web/my-financial-hub/src/commom/http/api.ts
+++ web/my-financial-hub/src/commom/http/api.ts
@@ -1,18 +1,20 @@
+import { ServiceResult } from '../interfaces/service-result';
+
 export default class Api<T>
 {
   private readonly _baseUrl: string;
   private readonly _baseEndpoint: string;
 
-  private get _apiUrl() : string {
+  private get _apiUrl(): string {
     return `${this._baseUrl}/${this._baseEndpoint}`;
-  }  
-
-  constructor(baseUrl: string,baseEndpoint: string) {
-    this._baseUrl       = baseUrl;
-    this._baseEndpoint  = baseEndpoint;
   }
 
-  async GetAllAsync(): Promise<T[]> {
+  constructor(baseUrl: string, baseEndpoint: string) {
+    this._baseUrl = baseUrl;
+    this._baseEndpoint = baseEndpoint;
+  }
+
+  async GetAllAsync(): Promise<ServiceResult<T[]>> {
     const result = await fetch(this._apiUrl);
     const json = await result.json();
 
@@ -20,10 +22,10 @@ export default class Api<T>
       throw json;//TODO:
     }
 
-    return json as T[];
+    return json as ServiceResult<T[]>;
   }
 
-  async PostAsync(body: T): Promise<T> {
+  async PostAsync(body: T): Promise<ServiceResult<T>> {
     const result = await fetch(this._apiUrl,
       {
         method: 'POST',
@@ -35,36 +37,35 @@ export default class Api<T>
     );
     const json = await result.json();
 
-    if (!result.ok) {
-      throw json;//TODO:
-    }
-
-    return json as T;
+    return json as ServiceResult<T>;
   }
 
-  async PutAsync(id: string,body: T): Promise<T> {
-    const result = await fetch(`${this._apiUrl}/${id}`,
-      {
-        method: 'PUT',
-        headers: {
-          'content-type': 'application/json'
-        },
-        body: JSON.stringify(body)
-      }
-    );
-    const json = await result.json();
+  async PutAsync(id: string, body: T): Promise<ServiceResult<T>> {
+    try {
+      const result = await fetch(`${this._apiUrl}/${id}`,
+        {
+          method: 'PUT',
+          headers: {
+            'content-type': 'application/json'
+          },
+          body: JSON.stringify(body)
+        }
+      );
+      const json = await result.json();
 
-    if (!result.ok) {
-      throw json;//TODO:
+      return json as ServiceResult<T>;
+    } catch (e) {
+      return {
+        hasError: true,
+        error: {
+          message: 'Internal Error'
+        }
+      } as ServiceResult<T>;
     }
-
-    console.log(json);
-
-    return json as T;
   }
 
-  async DeleteAsync(id: string): Promise<boolean>{
-    const result = await fetch(`${this._apiUrl}/${id}`,{ method: 'DELETE' });
+  async DeleteAsync(id: string): Promise<boolean> {
+    const result = await fetch(`${this._apiUrl}/${id}`, { method: 'DELETE' });
 
     if (!result.ok) {
       throw false;
