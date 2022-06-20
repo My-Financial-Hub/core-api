b8a3d7d786b6142cd129da4cc47ada0358b64751
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2022-06-20 16:27:03 -0300

    Add : Default Api Result

--- web/my-financial-hub/src/pages/accounts/contexts/accounts-page-context.tsx
+++ web/my-financial-hub/src/pages/accounts/contexts/accounts-page-context.tsx
@@ -12,8 +12,10 @@ const AccountContext = createContext<AccountsContext>(defaultContext);
 //EXPORT
 export function AccountsProvider({ children , defaultState}: { children?: JSX.Element | JSX.Element[] , defaultState?: AccountsState }) {
   const [state, setState] = useState({
-    accounts: defaultState?.accounts || defaultContextState.accounts, 
-    account:  defaultState?.account  || defaultContextState.account
+    hasError: defaultState?.hasError  || defaultContextState.hasError, 
+    error:    defaultState?.error     || defaultContextState.error, 
+    accounts: defaultState?.accounts  || defaultContextState.accounts, 
+    account:  defaultState?.account   || defaultContextState.account
   });
 
   return (
