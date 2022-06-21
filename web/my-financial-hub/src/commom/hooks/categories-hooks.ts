56b3f6311d99bbbf64e35ebc0cfd64b2eb38724a
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2022-06-21 17:25:33 -0300

    Add : categories get all hook

--- web/my-financial-hub/src/commom/hooks/categories-hooks.ts
+++ web/my-financial-hub/src/commom/hooks/categories-hooks.ts
@@ -1,9 +1,19 @@
+import { Dispatch, SetStateAction } from 'react';
 import CategoryApi from '../http/category-api';
 import { Category } from '../interfaces/category';
 
 export async function useCreateCategory(category: Category, api: CategoryApi) {
   try {
     await api.PostAsync(category);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function useGetAccounts(setState:Dispatch<SetStateAction<Category[]>>,api: CategoryApi) {
+  try {
+    const accountsResult = await api.GetAllAsync();
+    setState(accountsResult);
   } catch (error) {
     console.error(error);
   }
