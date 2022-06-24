ce3e48ed7ba275d20970f0d4aedc3d1c89bbdca1
Franklin Bitencourt <franklin.bdantas@hotmail.com>
2022-06-24 15:54:10 -0300

    Fix : account tests hooks

---
+++ web/my-financial-hub/src/__mocks__/hooks/accounts-page.hook.ts
@@ -0,0 +1,97 @@
+import { faker } from '@faker-js/faker';
+import { Account, defaultAccount } from '../../commom/interfaces/account';
+import * as hooks from '../../pages/accounts/hooks/accounts-page.hooks';
+
+const randTimeOut = faker.datatype.number(
+  {
+    min:500,
+    max:5000
+  }
+);
+
+export function mockUseGetAccounts(accounts?: Account[], timeout: number = randTimeOut) {
+  return jest.spyOn(hooks, 'useGetAccounts').mockImplementation(
+    async (context) => {
+      setTimeout(() => {
+        const [state, setState] = context;
+        if (accounts) {
+          setState({
+            ...state,
+            accounts
+          });
+          Promise.resolve();
+        } else {
+          Promise.reject('no accounts');
+        }
+      }, timeout);
+    }
+  );
+}
+
+export function mockUseDeleteAccount(){
+  return jest.spyOn(hooks, 'useDeleteAccount').mockImplementation(
+    async (context) => {
+      setTimeout(() => {
+        const [state, setState] = context;
+        setState(
+          {
+            ...state,
+            account: defaultAccount
+          }
+        );
+
+        Promise.resolve();
+      }, randTimeOut);
+    }
+  );
+}
+
+export function mockUseUpdateAccount(account?: Account){
+  const randTimeOut = faker.datatype.number(
+    {
+      min:500,
+      max:5000
+    }
+  );
+
+  return jest.spyOn(hooks, 'useUpdateAccount').mockImplementation(
+    async (context) => {
+      setTimeout(() => {
+        const [state, setState] = context;
+        setState(
+          {
+            ...state,
+            account: account ?? defaultAccount
+          }
+        );
+
+        Promise.resolve();
+      }, randTimeOut);
+    }
+  );
+}
+
+export function mockUseCreateAccount(account?: Account){
+  const randTimeOut = faker.datatype.number(
+    {
+      min:500,
+      max:5000
+    }
+  );
+
+  return jest.spyOn(hooks, 'useCreateAccount').mockImplementation(
+    async (context) => {
+      setTimeout(() => {
+        const [state, setState] = context;
+        setState(
+          {
+            ...state,
+            account: account ?? defaultAccount
+          }
+        );
+
+        Promise.resolve();
+      }, randTimeOut);
+    }
+  );
+}
