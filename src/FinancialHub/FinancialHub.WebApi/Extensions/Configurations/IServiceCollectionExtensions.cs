4b5430001d91bd62ef26587eff7be1461c5bf173
Franklin Bitencourt Dantas <franklin.bdantas@hotmail.com>
2023-03-20 21:42:29 -0300

    Changes Configuration for Infra.Data and Services project
    * Changes : IServiceCollectionExtensions for Infra and Services project
    
    * Removes : Action body for coverage

--- src/FinancialHub/FinancialHub.WebApi/Extensions/Configurations/IServiceCollectionExtensions.cs
+++ src/FinancialHub/FinancialHub.WebApi/Extensions/Configurations/IServiceCollectionExtensions.cs
@@ -1,17 +1,10 @@
-﻿using FinancialHub.Domain.Interfaces.Repositories;
-using FinancialHub.Domain.Interfaces.Services;
-using FinancialHub.Services.Services;
-using FinancialHub.Infra.Data.Repositories;
-
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
-using FinancialHub.Domain.Interfaces.Mappers;
-using FinancialHub.Services.Mappers;
 using FluentValidation;
-using FinancialHub.Domain.Mappers;
+using FluentValidation.AspNetCore;
+using Microsoft.OpenApi.Models;
 using FinancialHub.Domain.Models;
 using FinancialHub.WebApi.Validators;
-using FluentValidation.AspNetCore;
 
 namespace FinancialHub.WebApi.Extensions.Configurations
 {
@@ -26,31 +19,11 @@ namespace FinancialHub.WebApi.Extensions.Configurations
                 config.AssumeDefaultVersionWhenUnspecified = true;
                 config.ReportApiVersions = true;
             });
-            return services;
-        }
-        //TODO: find a better method organization
-        public static IServiceCollection AddRepositories(this IServiceCollection services)
-        {
-            services.AddScoped<IAccountsRepository, AccountsRepository>();
-            services.AddScoped<ICategoriesRepository, CategoriesRepository>();
-            services.AddScoped<ITransactionsRepository, TransactionsRepository>();
 
-            services.AddScoped<IBalancesRepository, BalancesRepository>();
-            return services;
-        }
-
-        public static IServiceCollection AddServices(this IServiceCollection services)
-        {
-            services.AddAutoMapper(typeof(FinancialHubAutoMapperProfile));
-            services.AddScoped<IMapperWrapper, FinancialHubMapperWrapper>();
-
-            services.AddScoped<IAccountsService, AccountsService>();
-            services.AddScoped<ICategoriesService, CategoriesService>();
-            services.AddScoped<ITransactionsService, TransactionsService>();
-            services.AddScoped<IBalancesService, BalancesService>();
-
-            services.AddScoped<IAccountBalanceService, AccountBalanceService>();
-            services.AddScoped<ITransactionBalanceService, TransactionBalanceService>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Financial Hub WebApi", Version = "v1" });
+            });
             return services;
         }
 
