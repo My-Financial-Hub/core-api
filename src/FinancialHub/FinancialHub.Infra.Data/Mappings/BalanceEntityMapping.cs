7aefc3505ea01625a5e03f90d11462343cea9e41
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2023-03-24 10:02:31 -0300

    Changes : Mapping for Transaction and Balance Entities

---
+++ src/FinancialHub/FinancialHub.Infra.Data/Mappings/BalanceEntityMapping.cs
@@ -0,0 +1,22 @@
+﻿using FinancialHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FinancialHub.Infra.Data.Mappings
+{
+    internal class BalanceEntityMapping : IEntityTypeConfiguration<BalanceEntity>
+    {
+        public void Configure(EntityTypeBuilder<BalanceEntity> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.HasIndex(t => t.Id).IsUnique(true);
+
+            builder.HasOne(x => x.Account)
+                    .WithMany(x => x.Balances)
+                    .HasForeignKey(x => x.AccountId)
+                    .HasPrincipalKey(x => x.Id)
+                    .IsRequired(true);
+            builder.Navigation(t => t.Account).AutoInclude();
+        }
+    }
+}
---
+++ src/FinancialHub/FinancialHub.Infra.Data/Mappings/TransactionEntityMapping.cs
@@ -0,0 +1,29 @@
+﻿using FinancialHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FinancialHub.Infra.Data.Mappings
+{
+    internal class TransactionEntityMapping : IEntityTypeConfiguration<TransactionEntity>
+    {
+        public void Configure(EntityTypeBuilder<TransactionEntity> table)
+        {
+            table.HasKey(t => t.Id);
+            table.HasIndex(t => t.Id).IsUnique(true);
+
+            table.HasOne(x => x.Balance)
+                .WithMany(x => x.Transactions)
+                .HasForeignKey(x => x.BalanceId)
+                .HasPrincipalKey(x => x.Id)
+                .IsRequired(true);
+            table.Navigation(t => t.Balance).AutoInclude();
+
+            table.HasOne(x => x.Category)
+                .WithMany(x => x.Transactions)
+                .HasForeignKey(x => x.CategoryId)
+                .HasPrincipalKey(x => x.Id)
+                .IsRequired(true);
+            table.Navigation(t => t.Category).AutoInclude();
+        }
+    }
+}
