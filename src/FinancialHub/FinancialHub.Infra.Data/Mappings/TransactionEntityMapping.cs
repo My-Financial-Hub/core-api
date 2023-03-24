d643e5e7c60f52e239c24d4bf4252ae702b7aa69
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2023-03-24 11:54:43 -0300

    Change : Moves notation from TransactionEntity to TransactionEntityMapping

--- src/FinancialHub/FinancialHub.Infra.Data/Mappings/TransactionEntityMapping.cs
+++ src/FinancialHub/FinancialHub.Infra.Data/Mappings/TransactionEntityMapping.cs
@@ -11,6 +11,21 @@ namespace FinancialHub.Infra.Data.Mappings
             table.HasKey(t => t.Id);
             table.HasIndex(t => t.Id).IsUnique(true);
 
+            table.Property(t => t.Description)
+                .HasColumnName("description")
+                .HasMaxLength(500);
+            table.Property(t => t.Amount)
+                .HasColumnType("money")
+                .IsRequired();
+            table.Property(t => t.TargetDate)
+                .HasColumnName("target_date")
+                .IsRequired();
+            table.Property(t => t.FinishDate)
+                .HasColumnName("finish_date");
+
+            table.Property(t => t.BalanceId)
+                .HasColumnName("balance_id")
+                .IsRequired(true);
             table.HasOne(x => x.Balance)
                 .WithMany(x => x.Transactions)
                 .HasForeignKey(x => x.BalanceId)
@@ -18,12 +33,27 @@ namespace FinancialHub.Infra.Data.Mappings
                 .IsRequired(true);
             table.Navigation(t => t.Balance).AutoInclude();
 
+            table.Property(t => t.CategoryId)
+                .HasColumnName("category_id")
+                .IsRequired(true);
             table.HasOne(x => x.Category)
                 .WithMany(x => x.Transactions)
                 .HasForeignKey(x => x.CategoryId)
                 .HasPrincipalKey(x => x.Id)
                 .IsRequired(true);
             table.Navigation(t => t.Category).AutoInclude();
+
+            table.Property(t => t.IsActive)
+                .HasColumnName("active")
+                .IsRequired(true);
+            table.Property(t => t.Status)
+                .HasColumnName("status")
+                .IsRequired(true);
+            table.Property(t => t.Type)
+                .HasColumnName("type")
+                .IsRequired(true);
+
+            table.ToTable("transactions");
         }
     }
 }
