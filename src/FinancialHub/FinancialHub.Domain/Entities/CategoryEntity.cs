111f6fcab1215dbbf7fd7a727bdb1b9348867bc9
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2023-03-27 20:19:14 -0300

    Adds : CategoryEntityMapping

--- src/FinancialHub/FinancialHub.Domain/Entities/CategoryEntity.cs
+++ src/FinancialHub/FinancialHub.Domain/Entities/CategoryEntity.cs
@@ -1,16 +1,11 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FinancialHub.Domain.Entities
 {
-    [Table("categories")]
     public class CategoryEntity : BaseEntity
     {
-        [Column("name", TypeName = "varchar(200)")]
         public string Name { get; set; }
-        [Column("description", TypeName = "varchar(500)")]
         public string Description { get; set; }
-        [Column("active")]
         public bool IsActive { get; set; }
 
         public ICollection<TransactionEntity> Transactions { get; set; }
