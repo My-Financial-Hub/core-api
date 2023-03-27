f7a3dd71cf90f2e1c2daa2312643597a4e8c9c23
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2023-03-27 20:08:34 -0300

    Adds: AccountEntityMapping

--- src/FinancialHub/FinancialHub.Domain/Entities/AccountEntity.cs
+++ src/FinancialHub/FinancialHub.Domain/Entities/AccountEntity.cs
@@ -1,16 +1,11 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FinancialHub.Domain.Entities
 {
-    [Table("accounts")]
     public class AccountEntity : BaseEntity
     {
-        [Column("name",TypeName = "varchar(200)")]
         public string Name { get; set; }
-        [Column("description", TypeName = "varchar(500)")]
         public string Description { get; set; }
-        [Column("active")]
         public bool IsActive { get; set; }
 
         public ICollection<BalanceEntity> Balances { get; set; }
