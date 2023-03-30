07252cd1f998f92bb9f2f96f651f4cac875f5349
Franklin Bitencourt Dantas <franklin.bdantas@hotmail.com>
2023-03-29 22:12:13 -0300

    Release v0.3.3 (#134)
    * Adds : CategoryEntityMapping, AccountEntityMapping, TransactionEntityMapping e BalanceEntityMapping
    * Changes Configuration for Infra.Data and Services project
    * Changes : IServiceCollectionExtensions for Infra and Services project
    * Removes : Action body for coverage

--- src/FinancialHub/FinancialHub.Infra.Data/Repositories/AccountsRepository.cs
+++ src/FinancialHub/FinancialHub.Infra.Data/Repositories/AccountsRepository.cs
@@ -1,11 +1,9 @@
 ﻿using FinancialHub.Domain.Entities;
 using FinancialHub.Domain.Interfaces.Repositories;
 using FinancialHub.Infra.Data.Contexts;
-using System.ComponentModel;
 
 namespace FinancialHub.Infra.Data.Repositories
 {
-    [Category("Repositories")]
     public class AccountsRepository : BaseRepository<AccountEntity>, IAccountsRepository
     {
         public AccountsRepository(FinancialHubContext context) : base(context)
