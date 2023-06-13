11886d9097a938364aaed08f3f67ad28226a266b
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2023-06-13 16:55:43 -0300

    Auth Application global Usings

---
+++ src/FinancialHub/FinancialHub.Auth.Application/Usings.cs
@@ -0,0 +1,5 @@
+﻿global using Microsoft.AspNetCore.Mvc;
+global using FinancialHub.Auth.Domain.Models;
+global using FinancialHub.Auth.Domain.Interfaces.Services;
+global using FinancialHub.Domain.Responses.Errors;
+global using FinancialHub.Domain.Responses.Success;
