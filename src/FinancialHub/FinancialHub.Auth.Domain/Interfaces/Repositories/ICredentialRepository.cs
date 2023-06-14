1370ec697166a9161646b49568925b8d376f246d
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2023-06-14 16:47:52 -0300

    Adds CredentialRepository

---
+++ src/FinancialHub/FinancialHub.Auth.Domain/Interfaces/Repositories/ICredentialRepository.cs
@@ -0,0 +1,14 @@
+﻿using FinancialHub.Auth.Domain.Entities;
+
+namespace FinancialHub.Auth.Domain.Interfaces.Repositories
+{
+    public interface ICredentialRepository
+    {
+        Task<CredentialEntity?> DeleteAsync(string username, string password);
+        Task<CredentialEntity?> DeleteAsync(Guid userId);
+        Task<CredentialEntity?> GetAsync(string username);
+        Task<CredentialEntity?> GetAsync(string username, string password); 
+        Task<CredentialEntity?> GetAsync(Guid userId);
+        Task<CredentialEntity> CreateAsync(CredentialEntity credential);
+    }
+}
---
+++ src/FinancialHub/FinancialHub.Auth.Infra.Data/Repositories/CredentialRepository.cs
@@ -0,0 +1,44 @@
+﻿namespace FinancialHub.Auth.Infra.Data.Repositories
+{
+    public class CredentialRepository : ICredentialRepository
+    {
+        private readonly FinancialHubAuthContext context;
+
+        public CredentialRepository(FinancialHubAuthContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<CredentialEntity> CreateAsync(CredentialEntity credential)
+        {
+            var entry = await this.context.AddAsync(credential);
+            await this.context.SaveChangesAsync();
+            return entry.Entity;
+        }
+
+        public async Task<CredentialEntity?> DeleteAsync(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CredentialEntity?> DeleteAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CredentialEntity?> GetAsync(string username)
+        {
+            return await this.context.Credentials.FirstOrDefaultAsync(x => x.Login == username);
+        }
+
+        public async Task<CredentialEntity?> GetAsync(string username, string password)
+        {
+            return await this.context.Credentials.FirstOrDefaultAsync(x => x.Login == username && x.Password == password);
+        }
+
+        public async Task<CredentialEntity> GetAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
