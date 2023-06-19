16a61e54b15e54700bf69a9198695b662f90ee43
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2023-06-19 17:34:28 -0300

    Changes ValidatorRulesExtensions

---
+++ src/FinancialHub/FinancialHub.Auth.Services/Validators/Rules/ValidatorRulesExtensions.cs
@@ -0,0 +1,32 @@
+﻿using FinancialHub.Auth.Domain.Interfaces.Resources;
+using FluentValidation;
+
+namespace FinancialHub.Auth.Services.Validators.Rules
+{
+    public static class ValidatorRulesExtensions
+    {
+        public static IRuleBuilderOptions<T, string> ValidEmail<T>(
+            this IRuleBuilderInitial<T, string> builder, IErrorMessageProvider provider
+        )
+        {
+            return builder
+                .NotEmpty()
+                .WithMessage(provider.Required)
+                .EmailAddress()
+                .WithMessage(provider.Invalid)
+                .MaximumLength(300)
+                .WithMessage(provider.MaxLength);
+        }
+
+        public static IRuleBuilderOptions<T, string> ValidName<T>(
+            this IRuleBuilderInitial<T, string> builder, IErrorMessageProvider provider
+        )
+        {
+            return builder
+                .NotEmpty()
+                .WithMessage(provider.Required)
+                .MaximumLength(300)
+                .WithMessage(provider.MaxLength);
+        }
+    }
+}
