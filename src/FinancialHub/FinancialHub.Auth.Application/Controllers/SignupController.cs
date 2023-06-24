9fcebfcb80d05fe6edd3cc8c848f9f06850d1cdf
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2023-06-24 17:20:37 -0300

    Adds SignupController

---
+++ src/FinancialHub/FinancialHub.Auth.Application/Controllers/SignupController.cs
@@ -0,0 +1,30 @@
+﻿namespace FinancialHub.Auth.Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class SignupController : ControllerBase
+    {
+        private readonly ISignupService signupService;
+
+        public SignupController(ISignupService signupService)
+        {
+            this.signupService = signupService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup([FromBody] SignupModel signup)
+        {
+            var result = await signupService.CreateAccountAsync(signup);
+
+            if(result.HasError)
+            {
+                return BadRequest(
+                    error: new ValidationErrorResponse(result.Error.Message)
+                );
+            }
+
+            return Ok(result);
+        }
+    }
+}
