44b7b0d36034ad893ca4763773a60f0226cf78c9
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2023-06-30 14:18:12 -0300

    SigninProvider unit tests

---
+++ src/FinancialHub/FinancialHub.Auth.Infra.Tests/Providers/Signin/SigninProviderTests.get_accounts.cs
@@ -0,0 +1,45 @@
+﻿namespace FinancialHub.Auth.Infra.Tests.Providers.Signin
+{
+    public partial class SigninProviderTests
+    {
+        [Test]
+        public async Task GetAccountAsync_ExistingCredential_ReturnsExistingUser()
+        {
+            var signin = builder.Generate();
+            var existingUser = userBuilder
+                .WithEmail(signin.Email)
+                .Generate();
+            var credential = credentialBuilder
+                .WithLogin(signin.Email)
+                .WithPassword(signin.Password)
+                .WithUserId(existingUser.Id)
+                .Generate();
+
+            credentialProvider
+                .Setup(x => x.GetAsync(It.Is<CredentialModel>(x => x.Login == signin.Email)))
+                .ReturnsAsync(credential);
+            userProvider
+                .Setup(x => x.GetAsync(existingUser.Id.GetValueOrDefault()))
+                .ReturnsAsync(existingUser);
+
+            var result = await provider.GetAccountAsync(signin);
+
+            Assert.That(result, Is.Not.Null);
+            ModelAssert.Equal(existingUser, result);
+        }
+
+        [Test]
+        public async Task GetAccountAsync_NotExistingCredential_ReturnsNull()
+        {
+            var signin = builder.Generate();
+            credentialProvider
+                .Setup(x => x.GetAsync(It.Is<CredentialModel>(x => x.Login == signin.Email)))
+                .ReturnsAsync(default(CredentialModel?))
+                .Verifiable();
+            var result = await provider.GetAccountAsync(signin);
+
+            Assert.That(result, Is.Null);
+            credentialProvider.Verify(x => x.GetAsync(It.Is<CredentialModel>(x => x.Login == signin.Email)), Times.Once);
+        }
+    }
+}
