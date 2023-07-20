d848eaec499292166041357ca34aa1a3986b2c71
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2023-07-20 19:05:01 -0300

    adds test controller

---
+++ src/FinancialHub/FinancialHub.Auth.Application/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace FinancialHub.Auth.Application.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult TestOk()
+        {
+            return Ok();
+        }
+    }
+}
