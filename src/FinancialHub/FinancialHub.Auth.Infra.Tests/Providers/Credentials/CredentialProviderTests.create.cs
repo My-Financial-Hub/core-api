7c10a9667e0b8f0b26d24844d5bfe075184c5926
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2023-07-27 17:21:50 -0300

    Removes exception unit tests of Provider layers

--- src/FinancialHub/FinancialHub.Auth.Infra.Tests/Providers/Credentials/CredentialProviderTests.create.cs
+++ src/FinancialHub/FinancialHub.Auth.Infra.Tests/Providers/Credentials/CredentialProviderTests.create.cs
@@ -17,18 +17,5 @@
             Assert.That(createdUser, Is.Not.Null);
             ModelAssert.Equal(credential, createdUser);
         }
-
-        [Test]
-        public void CreateAsync_RepositoryException_ThrowsException()
-        {
-            var credential = this.builder.Generate();
-            var exc = new Exception("Exception Message");
-
-            mockRepository
-                .Setup(x => x.CreateAsync(It.IsAny<CredentialEntity>()))
-                .ThrowsAsync(exc);
-
-            Assert.ThrowsAsync<Exception>(async () => await this.provider.CreateAsync(credential));
-        }
     }
 }
