d75a4ff5220a87b8c88a7594321eb48f877d4408
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2023-08-01 11:21:40 -0300

    Core Service global usinga

--- src/api/FinancialHub.Services/Usings.cs
+++ src/api/FinancialHub.Services/Usings.cs
@@ -1,9 +1,12 @@
-﻿global using FinancialHub.Domain.Interfaces.Repositories;
+﻿// Domain Project interfaces namespaces
+global using FinancialHub.Domain.Interfaces.Repositories;
 global using FinancialHub.Domain.Interfaces.Services;
 global using FinancialHub.Domain.Interfaces.Mappers;
 
+// Common Project namespaces
 global using FinancialHub.Common.Results;
 global using FinancialHub.Common.Results.Errors;
 
+// Domain Project entities & models namespaces
 global using FinancialHub.Domain.Entities;
 global using FinancialHub.Domain.Models;
