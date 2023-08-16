f2487c1f55f3526cd4ec02c4a405c6cd229ff98b
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2023-08-16 16:51:56 -0300

    AddsTransactionRepository.Delete unit tests

--- src/api/FinancialHub.Core.Infra.Data.NUnitTests/Repositories/Transactions/TransactionsRepositoryTests.delete.cs
+++ src/api/FinancialHub.Core.Infra.Data.NUnitTests/Repositories/Transactions/TransactionsRepositoryTests.delete.cs
@@ -3,7 +3,7 @@
     public partial class TransactionsRepositoryTests
     {
         [Test]
-        public async Task DeleteAsync_ValidItemWithNestChild_DoesNotDeleteNestChild()
+        public async Task DeleteAsync_DoesNotDeleteBalance()
         {
             var entity = this.GenerateObject();
 
@@ -17,7 +17,24 @@
             Assert.AreEqual(1,result);
 
             Assert.IsEmpty(this.context.Transactions.Local);
-            Assert.AreEqual(1, this.context.Accounts.Local.Count);
+            Assert.AreEqual(1, this.context.Balances.Local.Count);
+        }
+
+        [Test]
+        public async Task DeleteAsync_DoesNotDeleteCategory()
+        {
+            var entity = this.GenerateObject();
+
+            await this.InsertData(entity.Balance);
+            await this.InsertData(entity.Category);
+            await this.InsertData(entity);
+            this.context.ChangeTracker.Clear();
+
+            var result = await this.repository.DeleteAsync(entity.Id.Value);
+
+            Assert.AreEqual(1, result);
+
+            Assert.IsEmpty(this.context.Transactions.Local);
             Assert.AreEqual(1, this.context.Categories.Local.Count);
         }
     }
