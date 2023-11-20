4847ec39599d6892ef1c5c6e5ee3e7de546eeecf
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2023-11-20 19:56:41 -0300

    BalancesService tests mocks fixes

--- tests/core/FinancialHub.Core.Application.Tests/Services/Balances/BalancesServiceTests.cs
+++ tests/core/FinancialHub.Core.Application.Tests/Services/Balances/BalancesServiceTests.cs
@@ -1,9 +1,4 @@
-﻿using AutoMapper;
-using FinancialHub.Core.Domain.Interfaces.Mappers;
-using FinancialHub.Core.Domain.Interfaces.Repositories;
-using FinancialHub.Core.Domain.Interfaces.Services;
-using FinancialHub.Core.Domain.Tests.Builders.Entities;
-using FinancialHub.Core.Application.Mappers;
+﻿using FinancialHub.Core.Domain.Interfaces.Services;
 using FinancialHub.Core.Application.Services;
 using FinancialHub.Core.Domain.Interfaces.Providers;
 
@@ -12,7 +7,6 @@ namespace FinancialHub.Core.Application.Tests.Services
     public partial class BalancesServiceTests
     {
         protected Random random;
-        protected BalanceEntityBuilder balanceEntityBuilder;
         protected BalanceModelBuilder balanceModelBuilder;
 
         private IBalancesService service;
@@ -32,13 +26,7 @@ namespace FinancialHub.Core.Application.Tests.Services
 
             this.random = new Random();
 
-            this.balanceEntityBuilder = new BalanceEntityBuilder();
             this.balanceModelBuilder = new BalanceModelBuilder();
-        }
-
-        public ICollection<BalanceEntity> GenerateBalances()
-        {
-            return this.balanceEntityBuilder.Generate(random.Next(5, 10));
         }
     }
 }
