e4d13e15fffeeefa49c0d18ae1c054526699b4ec
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2024-02-01 00:31:27 -0300

    Removes AccountBalanceDto constructor

--- src/api/core/FinancialHub.Core.Domain/DTOS/Accounts/AccountBalanceDto.cs
+++ src/api/core/FinancialHub.Core.Domain/DTOS/Accounts/AccountBalanceDto.cs
@@ -6,13 +6,5 @@
         public string Currency { get; private set; }
         public decimal Amount { get; private set; }
         public bool IsActive { get; private set; }
-
-        public AccountBalanceDto(string name, string currency, decimal amount, bool isActive)
-        {
-            Name = name;
-            Currency = currency;
-            Amount = amount;
-            IsActive = isActive;
-        }
     }
 }
