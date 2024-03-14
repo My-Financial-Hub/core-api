6c9568128ef37172838e5b47c5b400871de87c4c
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2024-03-14 13:30:32 -0300

    Adds AccountsService.GetById unit tests

--- tests/core/FinancialHub.Core.Application.Tests/Services/Accounts/AccountsServiceTests.get.cs
+++ tests/core/FinancialHub.Core.Application.Tests/Services/Accounts/AccountsServiceTests.get.cs
@@ -5,7 +5,7 @@ namespace FinancialHub.Core.Application.Tests.Services
     public partial class AccountsServiceTests
     {
         [Test]
-        public async Task GetAllAsync_ValidUser_ReturnsAccounts()
+        public async Task GetAllAsync_ReturnsAccounts()
         {
             var entitiesMock = this.accountModelBuilder.Generate(10);
 
@@ -21,6 +21,47 @@ namespace FinancialHub.Core.Application.Tests.Services
             Assert.AreEqual(entitiesMock.Count, result.Data!.Count);
 
             this.provider.Verify(x => x.GetAllAsync(),Times.Once());
+        }
+
+        [Test]
+        public async Task GetByIdAsync_ExistingAccount_ReturnsAccount()
+        {
+            var id = Guid.NewGuid();
+            var entitiesMock = this.accountModelBuilder
+                .WithId(id)
+                .Generate();
+
+            this.validator
+                .Setup(x => x.ExistsAsync(id))
+                .ReturnsAsync(ServiceResult.Success);
+            this.provider
+                .Setup(x => x.GetByIdAsync(id))
+                .ReturnsAsync(entitiesMock);
+
+            var result = await this.service.GetByIdAsync(id);
+
+            Assert.IsFalse(result.HasError);
+            Assert.IsInstanceOf<ServiceResult<AccountDto>>(result);
+        }
+
+        [Test]
+        public async Task GetByIdAsync_NotExistingAccount_ReturnsNotFoundError()
+        {
+            var id = Guid.NewGuid();
+            var entitiesMock = this.accountModelBuilder
+                .WithId(id)
+                .Generate();
+            var expectedErrorMessage = $"Not found Account with id {id}";
+
+            this.validator
+                .Setup(x => x.ExistsAsync(id))
+                .ReturnsAsync(new NotFoundError(expectedErrorMessage));
+
+            var result = await this.service.GetByIdAsync(id);
+
+            Assert.IsFalse(result.HasError);
+            Assert.IsInstanceOf<NotFoundError>(result.Error);
+            Assert.AreEqual(expectedErrorMessage, result.Error!.Message);
         }
     }
 }
