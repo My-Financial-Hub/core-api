2ac5b52dfe930126fb1fb34e630417421a29d00b
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2024-03-27 21:04:34 -0300

    Adds IAccountCache

---
+++ src/api/core/FinancialHub.Core.Domain/Interfaces/Caching/IAccountCache.cs
@@ -0,0 +1,11 @@
+﻿using FinancialHub.Core.Domain.Models;
+
+namespace FinancialHub.Core.Domain.Interfaces.Caching
+{
+    public interface IAccountCache
+    {
+        Task AddAsync(Guid id, AccountModel account);
+        Task<AccountModel?> GetAsync(Guid id);
+        Task RemoveAsync(Guid id);
+    }
+}
