7b18a0f7330e193dff7d5c64738f0a5a5a5b0da0
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2024-03-27 21:04:34 -0300

    Adds IAccountCache

---
+++ src/api/core/FinancialHub.Core.Infra.Caching/Repositories/AccountCache.cs
@@ -0,0 +1,43 @@
+﻿using FinancialHub.Core.Infra.Caching.Extensions;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace FinancialHub.Core.Infra.Caching.Repositories
+{
+    public class AccountCache : IAccountCache
+    {
+        private readonly IDistributedCache cache;
+
+        public AccountCache(IDistributedCache cache)
+        {
+            this.cache = cache;
+        }
+
+        public async Task AddAsync(Guid id, AccountModel account)
+        {
+            await this.cache.SetAsync(
+                id.ToString(), 
+                account.ToByteArray(),
+                new DistributedCacheEntryOptions()
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMilliseconds(1000 * 60)
+                }
+            );
+        }
+
+        public async Task<AccountModel?> GetAsync(Guid id)
+        {
+            var result = await this.cache.GetAsync(id.ToString());
+            if(result == null)
+            {
+                return null;
+            }
+
+            return result.FromByteArray<AccountModel>();
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            await this.cache.RemoveAsync(id.ToString());
+        }
+    }
+}
