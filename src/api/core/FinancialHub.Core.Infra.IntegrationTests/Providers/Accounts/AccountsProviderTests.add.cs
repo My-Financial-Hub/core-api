99aefcf1c9b9cfd2405b15203574d543035fd5e4
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2024-05-08 12:15:08 -0300

    Adds AddAsync integration tests

---
+++ src/api/core/FinancialHub.Core.Infra.IntegrationTests/Providers/Accounts/AccountsProviderTests.add.cs
@@ -0,0 +1,45 @@
+﻿namespace FinancialHub.Core.Infra.IntegrationTests.Providers.Accounts
+{
+    public partial class AccountsProviderTests
+    {
+        [Test]
+        public async Task CreateAsync_AddsToDatabase()
+        {
+            var databaseAccountCount = database.Accounts.Count();
+            var account = this.builder.Generate();
+
+            await this.provider.CreateAsync(account);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(database.Accounts.Count(), Is.EqualTo(databaseAccountCount + 1));
+                Assert.That(
+                    database.Accounts.FirstOrDefault(x => 
+                        x.Name == account.Name && 
+                        x.Description == account.Description && 
+                        x.IsActive == account.IsActive
+                    ),
+                    Is.Not.Null
+                );
+            });
+        }
+
+        [Test]
+        public async Task CreateAsync_AddsToCache()
+        {
+            var account = this.builder.Generate();
+
+            await this.provider.CreateAsync(account);
+
+            var data = database.Accounts
+                .FirstOrDefault(x => 
+                    x.Name == account.Name && 
+                    x.Description == account.Description && 
+                    x.IsActive == account.IsActive
+                );
+            var cacheData = await cache.GetAsync($"accounts:{data!.Id}");
+            
+            Assert.That(cacheData, Is.Not.Empty);
+        }
+    }
+}
---
+++ src/api/core/FinancialHub.Core.Infra.IntegrationTests/Providers/Accounts/AccountsProviderTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using FinancialHub.Core.Infra.IntegrationTests.Setup;
+using FinancialHub.Core.Domain.Interfaces.Providers;
+using FinancialHub.Core.Infra.Data.Contexts;
+using FinancialHub.Core.Domain.Tests.Setup;
+using FinancialHub.Core.Domain.Tests.Builders.Models;
+
+namespace FinancialHub.Core.Infra.IntegrationTests.Providers.Accounts
+{
+    [TestFixtureSource(typeof(FinancialHubInfraFixture))]
+    public partial class AccountsProviderTests
+    {
+        private readonly IAccountsProvider provider;
+
+        private readonly AccountModelBuilder builder;
+
+        private readonly FinancialHubContext database;
+        private readonly IDistributedCache cache;
+
+        public AccountsProviderTests(FinancialHubInfraSetup setup, FinancialHubBuilderSetup builderSetup)
+        {
+            this.provider = setup.GetService<IAccountsProvider>();
+
+            this.cache = setup.GetService<IDistributedCache>();
+            this.database = setup.GetService<FinancialHubContext>();
+            this.builder = builderSetup.GetService<AccountModelBuilder>();
+        }
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            this.database.Database.EnsureCreated();
+        }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+            this.database.Database.EnsureDeleted();
+        }
+    }
+}
