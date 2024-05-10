bbdcc898b48b63c6487dc0ee2c8f82b6e41daa6a
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2024-05-10 11:07:48 -0300

    Adds InfraData Setup (Experimental)

---
+++ src/api/core/FinancialHub.Core.Infra.IntegrationTests/Setup/FinancialHubInfraDataSetup.cs
@@ -0,0 +1,48 @@
+﻿using Bogus.DataSets;
+using FinancialHub.Common.Entities;
+using FinancialHub.Core.Infra.Data.Extensions.Configurations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FinancialHub.Core.Infra.IntegrationTests.Setup
+{
+    public class FinancialHubInfraDataSetup : FinancialHubSetup
+    {
+        private readonly FinancialHubContext database;
+
+        public FinancialHubInfraDataSetup() : base()
+        {
+            services.AddRepositories(configuration);
+
+            serviceProvider = services.BuildServiceProvider();
+            
+            this.database   = serviceProvider.GetService<FinancialHubContext>()!;
+        }
+
+        public void CreateDatabase()
+        {
+            this.database.Database.EnsureCreated();
+        }
+
+        public void DropDatabase()
+        {
+            this.database.Database.EnsureDeleted();
+        }
+
+        public async Task<T> AddData<T>(T data) where T : BaseEntity
+        {
+            var context = this.serviceProvider.GetService<FinancialHubContext>()!;
+            var entry = await context.Set<T>().AddAsync(data);
+            await context.SaveChangesAsync();
+
+            context.ChangeTracker.Clear();
+            return entry.Entity;
+        }
+
+        public async Task<T?> GetDataById<T>(Guid id) where T : BaseEntity
+        {
+            var context = this.serviceProvider.GetService<FinancialHubContext>()!;
+            return await context.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
