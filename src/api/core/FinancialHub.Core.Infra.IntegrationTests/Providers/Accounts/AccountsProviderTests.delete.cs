aad0cf3c1d4236dae030128dc039f81e06744f99
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2024-05-10 11:07:29 -0300

    Adds AccountsProvider delete tests

---
+++ src/api/core/FinancialHub.Core.Infra.IntegrationTests/Providers/Accounts/AccountsProviderTests.delete.cs
@@ -0,0 +1,34 @@
+﻿namespace FinancialHub.Core.Infra.IntegrationTests.Providers.Accounts
+{
+    public partial class AccountsProviderTests
+    {
+        [Test]
+        public async Task DeleteAsync_RemovesFromDatabase()
+        {
+            var entity = this.entitybuilder.Generate();
+            await this.database.AddAsync(entity);
+            await this.database.SaveChangesAsync();
+            this.database.ChangeTracker.Clear();
+
+            await this.provider.DeleteAsync(entity.Id.GetValueOrDefault());
+
+            Assert.That(database.Accounts.Count(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public async Task DeleteAsync_RemovesFromCache()
+        {
+            var entity = this.entitybuilder.Generate();
+            await this.database.AddAsync(entity);
+            await this.database.SaveChangesAsync();
+            this.database.ChangeTracker.Clear();
+
+            var id = entity.Id.GetValueOrDefault();
+            await this.provider.DeleteAsync(id);
+
+            var cacheData = await cache.GetAsync($"accounts:{id}");
+
+            Assert.That(cacheData, Is.Null);
+        }
+    }
+}
