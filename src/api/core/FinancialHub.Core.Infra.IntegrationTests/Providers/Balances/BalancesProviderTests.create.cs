67b9fcaf7c98c54ebc354f8c58a7a9d5da91634b
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2024-06-05 10:27:14 -0300

    Adds BalanceProvider Integration tests

---
+++ src/api/core/FinancialHub.Core.Infra.IntegrationTests/Providers/Balances/BalancesProviderTests.create.cs
@@ -0,0 +1,80 @@
+﻿namespace FinancialHub.Core.Infra.IntegrationTests.Providers
+{
+    public partial class BalancesProviderTests
+    {
+        [Test]
+        public async Task CreateAsync_AddsToDatabase()
+        {
+            var balanceCount = database.Balances.Count();
+            var balance = this.builder.Generate();
+
+            await this.provider.CreateAsync(balance);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(database.Balances.Count(), Is.EqualTo(balanceCount + 1));
+                Assert.That(
+                    database.Balances.FirstOrDefault(x =>
+                        x.Name == balance.Name &&
+                        x.AccountId == balance.AccountId &&
+                        x.Currency == balance.Currency &&
+                        x.IsActive == balance.IsActive
+                    ),
+                    Is.Not.Null
+                );
+            });
+        }
+
+        [Test]
+        public async Task CreateAsync_NoAccountInCache_AddsBalanceToId()
+        {
+            var account = this.entitybuilder.Generate().Account;
+            await this.database.AddAsync(account);
+            await this.database.SaveChangesAsync();
+
+            var balance = this.builder
+                .WithAccountId(account.Id)
+                .Generate();
+
+            await this.provider.CreateAsync(balance);
+           
+            Assert.Fail();
+        }
+
+        [Test]
+        public async Task CreateAsync_ExistingAccountInCache_UpdatesBalances()
+        {
+            var account = this.entitybuilder.Generate().Account;
+            await this.database.AddAsync(account);
+            await this.database.SaveChangesAsync();
+
+            var balance = this.builder
+                .WithAccountId(account.Id)
+                .Generate();
+
+            await this.provider.CreateAsync(balance);
+
+            Assert.Fail();
+        }
+
+        [Test]
+        public async Task CreateAsync_NoAccount_DoNotAddBalanceToId()
+        {
+            var balance = this.builder.Generate();
+
+            await this.provider.CreateAsync(balance);
+        
+            Assert.Fail();
+        }
+
+        [Test]
+        public async Task CreateAsync_NoAccount_DoNotUpdateBalances()
+        {
+            var balance = this.builder.Generate();
+
+            await this.provider.CreateAsync(balance);
+        
+            Assert.Fail();
+        }
+    }
+}
---
+++ src/api/core/FinancialHub.Core.Infra.IntegrationTests/Providers/Balances/BalancesProviderTests.cs
@@ -0,0 +1,45 @@
+﻿using FinancialHub.Core.Domain.Interfaces.Caching;
+using FinancialHub.Core.Domain.Interfaces.Providers;
+using FinancialHub.Core.Domain.Interfaces.Repositories;
+
+namespace FinancialHub.Core.Infra.IntegrationTests.Providers
+{
+    [TestFixtureSource(typeof(FinancialHubInfraFixture))]
+    public partial class BalancesProviderTests
+    {
+        private readonly IBalancesProvider provider;
+
+        private readonly BalanceModelBuilder builder;
+        private readonly BalanceEntityBuilder entitybuilder;
+
+        private readonly IBalancesCache cache;
+        private readonly IBalancesRepository database;
+
+        private readonly FinancialHubInfraSetup setup;
+
+        public BalancesProviderTests(FinancialHubInfraSetup setup, FinancialHubBuilderSetup builderSetup)
+        {
+            this.setup = setup;
+
+            this.provider = setup.GetService<IBalancesProvider>();
+
+            this.cache      = setup.GetService<IBalancesCache>();
+            this.database  = setup.GetService<IBalancesRepository>();
+
+            this.builder = builderSetup.GetService<BalanceModelBuilder>();
+            this.entitybuilder = builderSetup.GetService<BalanceEntityBuilder>();
+        }
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            this.setup.TearUp();
+        }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+            this.setup.TearDown();
+        }
+    }
+}
