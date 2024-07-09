229de9e1062d3b88ef47ff5b00998922a3ffe23b
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2024-07-09 13:27:23 -0300

    Removes Domain.Balances

--- src/api/core/FinancialHub.Core.Domain.Balances/Enums/TransactionStatus.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FinancialHub.Core.Domain.Balances.Enums
-{
-    public enum TransactionStatus
-    {
-        NotCommitted = 0,
-        Committed = 1
-    }
-}
--- src/api/core/FinancialHub.Core.Domain.Balances/Enums/TransactionType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FinancialHub.Core.Domain.Balances.Enums
-{
-    public enum TransactionType
-    {
-        Earn = 1,
-        Expense = 2
-    }
-}
