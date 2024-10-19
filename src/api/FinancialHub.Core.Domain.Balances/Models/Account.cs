9ceaf06592a31cfe9f2490d7f2a5af73787f4036
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2024-10-19 19:50:05 -0300

    W.I.P.

---
+++ src/api/FinancialHub.Core.Domain.Balances/Models/Account.cs
@@ -0,0 +1,39 @@
+﻿namespace FinancialHub.Core.Domain.Accounts.Models
+{
+    public class Account
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool IsActive { get; private set; }
+
+        private ICollection<Balance> Balances { get; set; }
+
+        private Balance GetBalanceById(Guid balanceId)
+        {
+            return this.Balances.FirstOrDefault(x => x.Id == balanceId) ?? throw new IndexOutOfRangeException($"Balance with id {balanceId} not found");
+        }
+
+        public void AddBalance(string name, string currency, decimal amount)
+        {
+            this.Balances.Add(new Balance(Guid.NewGuid(), name, currency, amount, this.IsActive));
+        }
+
+        public void RemoveBalance(Guid balanceId)
+        {
+            this.Balances = this.Balances.Where(x => x.Id != balanceId).ToArray();
+        }
+
+        public void IncreaseAmount(Guid balanceId, decimal amount)
+        {
+            var balance = this.GetBalanceById(balanceId);
+            balance.IncreaseAmount(amount);
+        }
+
+        public void DecreaseAmount(Guid balanceId, decimal amount)
+        {
+            var balance = this.GetBalanceById(balanceId);
+            balance.DecreaseAmount(amount);
+        }
+    }
+}
---
+++ src/api/FinancialHub.Core.Domain.Balances/Models/Balance.cs
@@ -0,0 +1,34 @@
+﻿namespace FinancialHub.Core.Domain.Accounts.Models
+{
+    internal class Balance
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Currency { get; private set; }
+        public decimal Amount { get; private set; }
+        public bool IsActive { get; private set; }
+
+        public Balance(Guid id, string name, string currency, decimal amount, bool isActive)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Currency = currency;
+            this.Amount = amount;
+            this.IsActive = isActive;
+        }
+
+        public void IncreaseAmount(decimal amount) 
+        {
+            this.Amount += amount;
+        }
+
+        public void DecreaseAmount(decimal amount) 
+        { 
+            if(this.Amount - amount < 0)
+            {
+                throw new ArgumentException("Amount cannot be smaller than zero",nameof(amount));
+            }
+            this.Amount -= amount;
+        }
+    }
+}
