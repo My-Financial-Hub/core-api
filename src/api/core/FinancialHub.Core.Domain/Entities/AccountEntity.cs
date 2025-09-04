683c84c19bfa2a9608099f6c64fb2f16b7565755
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-09-04 15:36:21 -0300

    Adds Nullable to Description properties

--- src/api/core/FinancialHub.Core.Domain/Entities/AccountEntity.cs
+++ src/api/core/FinancialHub.Core.Domain/Entities/AccountEntity.cs
@@ -3,7 +3,7 @@
     public class AccountEntity : BaseEntity
     {
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public bool IsActive { get; set; }
 
         public ICollection<BalanceEntity> Balances { get; set; }
